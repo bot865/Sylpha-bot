c871982d950a4637d54fd6446e971479f25a9c99
bot865 <111519198+bot865@users.noreply.github.com>
2024-07-05 09:58:19 +0300

    Update dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,7 +1,7 @@
 import Starlights from '@StarlightsTeam/Scraper'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat, '*`Ingresa el enlace del vídeo de Instagram junto al comando.`*', m)
+if (!args[0]) return conn.reply(m.chat, '*`أدخل مقطع فيديو Instagram إلى جانب الأمر 🐍♥.`*', m)
 try {
 let { dl_url } = await Starlights.igdl(args[0])
 await conn.sendFile(m.chat, dl_url, 'igdl.mp4', null, m, null, rcanal)
@@ -9,6 +9,6 @@ await conn.sendFile(m.chat, dl_url, 'igdl.mp4', null, m, null, rcanal)
 }}
 handler.help = ['instagram *<link>*']
 handler.tags = ['dl']
-handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i
+handler.command = /^(انستا|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i
 
 export default handler
